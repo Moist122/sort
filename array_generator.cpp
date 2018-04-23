d3f74d0e2447a04b152b6500410fc76a8cb1ee9a
Moist122 <milosz.chlebowski97@gmail.com>
2018-04-23 17:52:41 +0200

    array generator

---
+++ array_generator.cpp
@@ -0,0 +1,17 @@
+#include<fstream> //c++11
+
+const int elem_num=50000; //specify array size
+//create 100 arrays and save to file
+int main() {
+    srand (time(NULL));
+    for(int i=0;i<100;++i) {
+        int array[elem_num];
+        for(int j=0;j<elem_num;++j)
+            array[j] = rand();
+        std::ofstream myfile (std::to_string(elem_num)+"/"+std::to_string(i)+".txt");
+        if (myfile.is_open())
+            for(int j=0;j<elem_num;++j)
+                myfile<<array[j]<<' ';
+        myfile.close();
+    }
+}
